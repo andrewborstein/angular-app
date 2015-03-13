49df7e30fcbbcdf9718f7f9656f044ef9f2a28e5
Andrew Borstein <info@brownborgstudios.com>
2015-03-13 07:30:13 -0400

    store.config --> app.config

--- app.js
+++ app.js
@@ -58,7 +58,7 @@
   // var app = angular.module('application name', [dependencies]);
   var app = angular.module('store', ['store-products']);
 
-  store.config(['$httpProvider', function($httpProvider) {
+  app.config(['$httpProvider', function($httpProvider) {
    $httpProvider.defaults.useXDomain = true;
    delete $httpProvider.defaults.headers.common['X-Requested-With'];
    }
