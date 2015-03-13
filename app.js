84043b14e473bf5dc9b3a87cd484355692ded84a
Andrew Borstein <info@brownborgstudios.com>
2015-03-12 21:51:29 -0400

    Simplify API URL

--- app.js
+++ app.js
@@ -66,7 +66,7 @@
     api_key = '3ugcu6nyygcbysomqa2ed2ja';
     etsyURL = 'https://openapi.etsy.com/v2/shops/'+shop_id+'/listings/active.js?method=GET&api_key='+api_key+'&fields=title,url,price,quantity,description&limit=100&includes=MainImage,'
 
-    $http.jsonp(etsyURL).
+    $http.jsonp('https://openapi.etsy.com/v2/shops/fiddlefishstore/').
       success(function(data,status,headers,config){
         // store.products = data;
         // store.products = gems;
