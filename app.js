f6ffcf55a38334c87f66cac1695f91f01fa5a04e
Andrew Borstein <info@brownborgstudios.com>
2015-03-12 21:39:32 -0400

    .success fix

--- app.js
+++ app.js
@@ -60,14 +60,14 @@
 
   // controller for the whole store
   app.controller('StoreController', [ '$http', function($http) {
+    
     var store = this;
     store.products = [];
     shop_id = 'fiddlefishstore'
     api_key = '3ugcu6nyygcbysomqa2ed2ja';
     etsyURL = 'https://openapi.etsy.com/v2/shops/'+shop_id+'/listings/active.js?method=GET&api_key='+api_key+'&fields=title,url,price,quantity,description&limit=100&includes=MainImage,'
 
-    $http.jsonp(etsyURL)
-      .success(function(data){
+    $http.jsonp(etsyURL).success(function(data){
         if (data.ok) {
           store.products = data;
           console.log(data);
@@ -75,6 +75,7 @@
           alert(data.error);
         }
       });
+
   }]);
 
   // controller for the reviews
