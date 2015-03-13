fc7e00b2f04de3d268089dde2fb9df8867cae6de
Andrew Borstein <info@brownborgstudios.com>
2015-03-13 07:31:11 -0400

    more .config settings for cross origin

--- app.js
+++ app.js
@@ -59,8 +59,11 @@
   var app = angular.module('store', ['store-products']);
 
   app.config(['$httpProvider', function($httpProvider) {
-   $httpProvider.defaults.useXDomain = true;
-   delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.withCredentials = true;
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
    }
   ]);
 
