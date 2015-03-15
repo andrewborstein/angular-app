e80b26d79e13bab101062b02a8472e4802332bb6
Andrew Borstein <info@brownborgstudios.com>
2015-03-15 17:08:25 -0400

    Move etsy module inside store app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // wrap the whole thing in an empty function
 (function() {
 
+  /*
   shop_id = 'fiddlefishstore'
   api_key = '3ugcu6nyygcbysomqa2ed2ja';
   etsyURL = 'https://openapi.etsy.com/v2/shops/'+shop_id+'/listings/active.js?method=GET&api_key='+api_key+'&fields=title,url,price,quantity,description&limit=100&includes=MainImage,'
@@ -52,22 +53,45 @@
       }
     }
   });
+  */
 
   // var app = angular.module('application name', [dependencies]);
   var app = angular.module('store', ['store-products']);
 
-  // controller for the details panel
-  /*
-  app.controller('PanelController', function(){
-    this.tab = 1;
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    };
-  });
-  */
+  // ETSY API Request
+  app.controller('EtsyController', ['$scope', '$http',
+      function($scope, $http) {
+
+        shop_id = 'fiddlefishstore'
+        api_key = '&api_key=3ugcu6nyygcbysomqa2ed2ja';
+        fields = '&fields=title,url,price,quantity,description';
+        limit = '&limit=100';
+        includes = '&includes=MainImage';
+        callback = '&callback=JSON_CALLBACK';
+        etsyURL = 'https://openapi.etsy.com/v2/shops/'
+          +shop_id
+          +'/listings/active.js?method=GET'
+          +api_key
+          +fields
+          +limit
+          +includes
+          +callback    
+
+        $http.jsonp(etsyURL).
+          success(function(data, status) {
+            $scope.status = status;
+            $scope.data = data;
+            console.log('status:');
+            console.log(status);
+            console.log('data response:');
+            console.log(data);
+          }).
+          error(function(data, status) {
+            $scope.data = data || "Request failed";
+            $scope.status = status;
+        });
+      
+  }]);
 
   // controller for the image gallery
   app.controller('GalleryController', function(){
