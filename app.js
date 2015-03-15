96491f3f24471708f87a72e59c09e79d556963a9
Andrew Borstein <info@brownborgstudios.com>
2015-03-15 17:33:35 -0400

    data.results on index page & $http in store controller

--- app.js
+++ app.js
@@ -60,36 +60,36 @@
 
   // ETSY API Request
   app.controller('EtsyController', ['$scope', '$http',
-      function($scope, $http) {
+    function($scope, $http) {
 
-        shop_id = 'fiddlefishstore'
-        api_key = '&api_key=3ugcu6nyygcbysomqa2ed2ja';
-        fields = '&fields=title,url,price,quantity,description';
-        limit = '&limit=100';
-        includes = '&includes=MainImage';
-        callback = '&callback=JSON_CALLBACK';
-        etsyURL = 'https://openapi.etsy.com/v2/shops/'
-          +shop_id
-          +'/listings/active.js?method=GET'
-          +api_key
-          +fields
-          +limit
-          +includes
-          +callback    
+      shop_id = 'fiddlefishstore'
+      api_key = '&api_key=3ugcu6nyygcbysomqa2ed2ja';
+      fields = '&fields=title,url,price,quantity,description';
+      limit = '&limit=100';
+      includes = '&includes=MainImage';
+      callback = '&callback=JSON_CALLBACK';
+      etsyURL = 'https://openapi.etsy.com/v2/shops/'
+        +shop_id
+        +'/listings/active.js?method=GET'
+        +api_key
+        +fields
+        +limit
+        +includes
+        +callback    
 
-        $http.jsonp(etsyURL).
-          success(function(data, status) {
-            $scope.status = status;
-            $scope.data = data;
-            console.log('status:');
-            console.log(status);
-            console.log('data response:');
-            console.log(data);
-          }).
-          error(function(data, status) {
-            $scope.data = data || "Request failed";
-            $scope.status = status;
-        });
+      $http.jsonp(etsyURL).
+        success(function(data, status, results) {
+          $scope.status = status;
+          $scope.data = data;
+          console.log('status:');
+          console.log(status);
+          console.log('data response:');
+          console.log(data);
+        }).
+        error(function(data, status) {
+          $scope.data = data || "Request failed";
+          $scope.status = status;
+      });
       
   }]);
 
@@ -105,9 +105,40 @@
   });
 
   // controller for the store
-  app.controller('StoreController', function(){
-    this.products = gems;
-  });
+  app.controller('StoreController', ['$scope', '$http',
+    function($scope, $http) {
+      
+      shop_id = 'fiddlefishstore'
+      api_key = '&api_key=3ugcu6nyygcbysomqa2ed2ja';
+      fields = '&fields=title,url,price,quantity,description';
+      limit = '&limit=100';
+      includes = '&includes=MainImage';
+      callback = '&callback=JSON_CALLBACK';
+      etsyURL = 'https://openapi.etsy.com/v2/shops/'
+        +shop_id
+        +'/listings/active.js?method=GET'
+        +api_key
+        +fields
+        +limit
+        +includes
+        +callback    
+
+      $http.jsonp(etsyURL).
+        success(function(data, status, results) {
+          $scope.status = status;
+          $scope.data = data;
+          console.log('status:');
+          console.log(status);
+          console.log('data response:');
+          console.log(data);
+        }).
+        error(function(data, status) {
+          $scope.data = data || "Request failed";
+          $scope.status = status;
+      });
+
+      this.products = gems;
+  }]);
 
   // controller for the reviews
   app.controller('ReviewController', function(){
