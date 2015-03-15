fe7ddd73f3dcf29c3769b35ab913e3f34fdb863d
Andrew Borstein <info@brownborgstudios.com>
2015-03-15 17:01:06 -0400

    Revert 7fc2313..31469c4
    This rolls back to commit 7fc2313e6811361c80cd59acb8946152285a6dc4.

--- app.js
+++ app.js
@@ -1,52 +1,89 @@
 // wrap the whole thing in an empty function
 (function() {
 
+  shop_id = 'fiddlefishstore'
+  api_key = '3ugcu6nyygcbysomqa2ed2ja';
+  etsyURL = 'https://openapi.etsy.com/v2/shops/'+shop_id+'/listings/active.js?method=GET&api_key='+api_key+'&fields=title,url,price,quantity,description&limit=100&includes=MainImage,'
+
+  var html = "";
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    beforeSend: function() {
+      $('#etsy').addClass('loading'); // Show loader icon
+    },
+    complete: function() {
+      $('#etsy').removeClass('loading'); // Hide loader icon
+    },
+    success: function(data) {
+      if (data.ok) {
+        console.log(data);
+        console.log(data.results);
+        console.log(data.results[0].title);
+
+        var notyet = ''
+        
+        // Total number of items in your shop
+        var listingsTotal = 'You have '+data.count+' products in your shop';
+
+        $.each( data.results, function ( i, item ) {
+        // Specific Listing Title
+        var image = this.MainImage.url_570xN;
+        var title = this.title;
+        var url = this.url;
+        var price = this.price;
+        var quantity = this.quantity;
+        var id = this.listing_id;
+        var desc = this.description;
+        html += '<li class="item">' + 
+                '<a href="' + url + '">' +
+                  '<img class="avatar" src="' + image + '">' + 
+                  '<h4 class="title">' + title + '</h4>' + 
+                  '</a>' +
+                  '<p class="desc">' + desc + '</p>' +
+                  '<p class="price"><strong>Price</strong>: $' + price + '</p>' +
+                  '<p class="quantity"><strong>Quantity</strong>: ' + quantity + '</p>' + 
+                '</li>';
+        });
+        $( '.total' ).prepend(listingsTotal);
+        $( '.items' ).append(html);
+      } else {
+        alert(data.error);
+      }
+    }
+  });
+
   // var app = angular.module('application name', [dependencies]);
   var app = angular.module('store', ['store-products']);
 
-  // controller for the whole store
-  app.controller('StoreController', function() {
-    //var store = this;
-    //store.products = [];
-    this.products = gems;
+  // controller for the details panel
+  /*
+  app.controller('PanelController', function(){
+    this.tab = 1;
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+  */
+
+  // controller for the image gallery
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(newGallery) {
+      this.current = newGallery || 0;
+    };
+    this.isCurrent = function(newGallery) {
+      return this.current === checkGallery;
+    };
   });
 
-  // ETSY API Request
-  angular.module('etsy', [])
-    .controller('FetchController', ['$scope', '$http',
-      function($scope, $http) {
-
-        shop_id = 'fiddlefishstore'
-        api_key = '&api_key=3ugcu6nyygcbysomqa2ed2ja';
-        fields = '&fields=title,url,price,quantity,description';
-        limit = '&limit=100';
-        includes = '&includes=MainImage';
-        callback = '&callback=JSON_CALLBACK';
-        etsyURL = 'https://openapi.etsy.com/v2/shops/'
-          +shop_id
-          +'/listings/active.js?method=GET'
-          +api_key
-          +fields
-          +limit
-          +includes
-          +callback    
-
-        $http.jsonp(etsyURL).
-          success(function(data, status) {
-            $scope.status = status;
-            $scope.data = data;
-            console.log('status:');
-            console.log(status);
-            console.log('data response:');
-            console.log(data);
-          }).
-          error(function(data, status) {
-            $scope.data = data || "Request failed";
-            $scope.status = status;
-        });
-      
-  }]);
-
+  // controller for the store
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
 
   // controller for the reviews
   app.controller('ReviewController', function(){
